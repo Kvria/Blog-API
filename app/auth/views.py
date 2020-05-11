f5b60ff60de1ffe09b5283c246b5bfaa59cab9b9
Kvria <trishakuria@gmail.com>
2020-05-11 06:53:35 +0300

    add imports in views file

---
+++ app/auth/views.py
@@ -0,0 +1,6 @@
+from flask import render_template,redirect,url_for,flash,request
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from flask_login import login_user,logout_user,login_required
+from .. import db
+from .import auth
